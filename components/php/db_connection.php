88fca94b872b971411ce727c4c7166068e868bfa
JorgeMiraFlores <130872239+JorgeMiraFlores@users.noreply.github.com>
2024-03-17 20:57:51 -0600

    Funcion conexion() agregada

--- components/php/db_connection.php
+++ components/php/db_connection.php
@@ -7,7 +7,15 @@ $dbname = "app_notas";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa";
+   // echo "Conexión exitosa";
 } catch(PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
+function conexion(){
+    $mysqli_conexion = new mysqli("localhost", "root", "", "app_notas");
+    if($mysqli_conexion->connect_errno){
+        echo "Fallo al conectar a MySQL: (" . $mysqli_conexion->connect_errno . ") " . $mysqli_conexion->connect_error;
+    }
+    return $mysqli_conexion;
+}
+?>
