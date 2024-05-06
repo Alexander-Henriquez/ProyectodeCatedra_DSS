279bb8891d2a8fc757108e9cbe6ccf4e075b26b5
Oswaldo Henríquez <132530276+Alexander-Henriquez@users.noreply.github.com>
2024-05-06 01:14:34 -0600

    Update crear_recordatorio.php

--- components/php/crear_recordatorio.php
+++ components/php/crear_recordatorio.php
@@ -8,9 +8,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Recupera los datos del formulario enviado por el método POST
     $nota_id = $_POST["nota_id"];
-$fecha_recordatorio = $_POST["fecha_recordatorio"];
-$titulo = $_POST["titulo"];
-$contenido = $_POST["contenido"];
+    $titulo = $_POST["titulo"];
+    $contenido = $_POST["contenido"];
+    $color_fondo = $_POST["color_fondo"];
+    $fecha_recordatorio = $_POST["fecha_recordatorio"];
+    $hora_recordatorio = $_POST["hora_recordatorio"];
 
     if (isset($_SESSION['id'])) {
         $usuario_id = $_SESSION['id'];
@@ -21,14 +23,16 @@ $contenido = $_POST["contenido"];
 
     try {
         // Prepara la consulta SQL para insertar un nuevo recordatorio en la base de datos
-        $stmt = $conn->prepare("INSERT INTO recordatorios (usuario_id, nota_id, fecha_recordatorio, titulo, contenido) VALUES (:usuario_id, :nota_id, :fecha_recordatorio, :titulo, :contenido)");
+        $stmt = $conn->prepare("INSERT INTO recordatorios (usuario_id, nota_id, titulo, contenido, fecha_recordatorio, hora_recordatorio, color_fondo) VALUES (:usuario_id, :nota_id, :titulo, :contenido, :fecha_recordatorio, :hora_recordatorio, :color_fondo)");
     
         // Vincula los parámetros de la consulta con las variables obtenidas del formulario
         $stmt->bindParam(':usuario_id', $usuario_id);
         $stmt->bindParam(':nota_id', $nota_id);
-        $stmt->bindParam(':fecha_recordatorio', $fecha_recordatorio);
         $stmt->bindParam(':titulo', $titulo);
         $stmt->bindParam(':contenido', $contenido);
+        $stmt->bindParam(':color_fondo', $color_fondo);
+        $stmt->bindParam(':fecha_recordatorio', $fecha_recordatorio);
+        $stmt->bindParam(':hora_recordatorio', $hora_recordatorio);
         
         // Ejecuta la consulta SQL
         $stmt->execute();
